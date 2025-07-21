94ea93f85c4b447a11f23ee7f6732c26abf2b700
johndupls <john@dupls.com>
2025-07-21 15:45:46 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,2 +1,17 @@
-WIFI_NAME = 'xxxxx'
-WIFI_PASS = "xxxxx"
+# WiFI
+WIFI_NAME = 'xxxxxx'
+WIFI_PASS = "xxxxxxxxxxx"
+
+# Gmail
+sender_email = 'xxxxxxxxxxxxxx'
+sender_name = 'xxxxxxx'
+sender_app_password = 'xxxxxxxxxxx'
+recipient_email = 'xxxxxxxxxxxxx'
+
+# SMTP
+HOST_NAME = 'xxxxxxxxx'
+PORT_NUM = xxx
+
+# Terminal Specific
+SUMP_PUMP_NUMBER = 'x'
+STATIC_ADDR = 'xxx.xxx.x.xx'
